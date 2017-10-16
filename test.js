916aab5238114ed30f55af12037a7b21a347a770
astur <astur@yandex.ru>
2017-10-16 17:31:38 +0300

    First tests

--- test.js
+++ test.js
@@ -1,6 +1,24 @@
 const test = require('ava');
-const m = require('.');
+const getProps = require('.');
 
 test('Base', t => {
-    t.is(typeof m, 'function');
+    t.is(typeof getProps, 'function');
+});
+
+test('Enums', t => {
+    t.is(getProps([], {enums: false}).indexOf('length'), -1);
+    t.not(getProps([]).indexOf('length'), -1);
+});
+
+test('Protos', t => {
+    t.is(getProps([]).indexOf('map'), -1);
+    t.not(getProps([], {protos: true}).indexOf('map'), -1);
+});
+
+test('Symbols', t => {
+    const s = Symbol('1');
+    const a = [1];
+    a[s] = 1;
+    t.is(getProps(a).indexOf(s), -1);
+    t.not(getProps(a, {symbols: true}).indexOf(s), -1);
 });
