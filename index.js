c83bbc74e3a3bd9f54caba49f7e7a8b06be84fc0
astur <astur@yandex.ru>
2017-10-16 17:32:08 +0300

    First implementation

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-module.exports = {};
+module.exports = (obj, config) => {
+    const {
+        enums,
+        protos,
+        symbols,
+    } = Object.assign({
+        enums: true,
+        protos: false,
+        symbols: false,
+    }, config);
+
+    const isEnum = Object.prototype.propertyIsEnumerable.bind(obj);
+    const props = [];
+    do {
+        const keys = [];
+        keys.push(...Object.getOwnPropertyNames(obj));
+        if(symbols){
+            keys.push(...Object.getOwnPropertySymbols(obj));
+        }
+        props.push(...enums ? keys : keys.filter(key => isEnum(key)));
+        obj = protos ? Object.getPrototypeOf(obj) : false;
+    } while(obj);
+
+    return [...new Set(props)];
+};
